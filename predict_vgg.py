1db9b5f70200dfbb4e26a0f87a6b9664471594c3
Azoffsky <azoffsky@yahoo.com>
2022-09-04 08:41:18 +0300

    Signal class prediction (CNN)

---
+++ predict_vgg.py
@@ -0,0 +1,37 @@
+# USAGE
+
+# python predict_vgg.py --image images\chirp_55.jpg --model output\vgg_model.model --label-bin output\vgg_lb.pickle --width 64 --height 64
+
+from tensorflow.keras.models import load_model
+import argparse
+import pickle
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True)
+ap.add_argument("-m", "--model", required=True)
+ap.add_argument("-l", "--label-bin", required=True)
+ap.add_argument("-w", "--width", type=int, default=28)
+ap.add_argument("-e", "--height", type=int, default=28)
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+output = image.copy()
+image = cv2.resize(image, (args["width"], args["height"]))
+image = image.astype("float") / 255.0
+image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+
+model = load_model(args["model"])
+lb = pickle.loads(open(args["label_bin"], "rb").read())
+
+preds = model.predict(image)
+print(preds)
+
+i = preds.argmax(axis=1)[0]
+label = lb.classes_[i]
+
+text = "{}: {:.2f}%".format(label, preds[0][i] * 100)
+cv2.putText(output, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+cv2.imshow("Output", output)
+cv2.waitKey(0)
